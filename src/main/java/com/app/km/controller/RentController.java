0f3f7b9940d64736d8f7c8d8f65178f669f7e930
RangerBlue <kamil.machul@gmail.com>
2017-05-19 00:17:51 +0200

    dodano resty do rent i plik sql

--- src/main/java/com/app/km/controller/RentController.java
+++ src/main/java/com/app/km/controller/RentController.java
@@ -5,13 +5,11 @@ import com.app.km.request.RentRequest;
 import com.app.km.respository.CarRepository;
 import com.app.km.respository.RentRepository;
 import com.app.km.respository.UsersRepository;
+import com.app.km.util.CustomErrorType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.Timestamp;
 import java.util.List;
@@ -44,14 +42,61 @@ public class RentController {
             return new ResponseEntity(rents, HttpStatus.OK);
     }
 
+    //select where id
+    @RequestMapping(value="/{id}", method = RequestMethod.GET)
+    public ResponseEntity<?> getRentWhereId(@PathVariable int id){
+        RentEntity user = rentRepository.findOne(id);
+        if(user == null)
+            return new ResponseEntity(new CustomErrorType("Rent with id "+id+" not found"),
+                    HttpStatus.NOT_FOUND);
+        else
+            return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    //update
+    @RequestMapping(value="/{id}", method = RequestMethod.POST)
+    public ResponseEntity<?> updateRent(@PathVariable int id, @RequestBody RentRequest rent){
+        RentEntity currentRent = rentRepository.findOne(id);
+        if(currentRent == null)
+            return new ResponseEntity(new CustomErrorType("Unable to update rent with id "+id), HttpStatus.NOT_FOUND);
+        else{
+            currentRent.setStart(rent.getStart());
+            currentRent.setEnd(rent.getEnd());
+            currentRent.setUserEntity(userRepository.findOne(rent.getUsers_iduser()));
+            currentRent.setCarEntity(carRepository.findOne(rent.getCar_idcar()));
+            return new ResponseEntity<>(currentRent, HttpStatus.OK);
+        }
+
+    }
+
     //insert
     @RequestMapping(method = RequestMethod.POST)
-    public void addUsers(@RequestBody RentRequest addRentRequest){
+    public void addRent(@RequestBody RentRequest addRentRequest){
         RentEntity rent = new RentEntity();
         rent.setStart(new Timestamp(System.currentTimeMillis()));
         rent.setUserEntity(userRepository.findOne(addRentRequest.getUsers_iduser()));
         rent.setCarEntity(carRepository.findOne(addRentRequest.getCar_idcar()));
         rentRepository.save(rent);
+    }
+
+    //delete
+    @RequestMapping(value="/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<?> deleteRent(@PathVariable int id){
+        RentEntity rent = rentRepository.findOne(id);
+        if(rent == null)
+            return new ResponseEntity(new CustomErrorType("Unable to delete rent with id "+id), HttpStatus.NOT_FOUND);
+        else{
+            rentRepository.delete(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+
+    }
+
+    //delete all
+    @RequestMapping(method = RequestMethod.DELETE)
+    public ResponseEntity<?> deleteAllRents(){
+        rentRepository.deleteAll();
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 }
